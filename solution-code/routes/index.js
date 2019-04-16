2e4c0bb8bcc48b0e53933d5283994dd317c31a74
Maxence Bouret <maxence.bouret@gmail.com>
2019-04-16 09:39:15 +0100

    Copy of instructions

--- solution-code/routes/index.js
+++ solution-code/routes/index.js
@@ -1,9 +1,52 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router  = express.Router()
+const Tshirt = require('../models/Tshirt')
 
-/* GET home page */
+// Home page
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
-module.exports = router;
+
+// Page to display all tshirts
+router.get('/tshirts', (req,res,next)=>{
+  Tshirt.find({ isValidated: true })
+  .then(tshirts => {
+    res.render('tshirts', { tshirts })
+  })
+})
+
+
+
+// TODO: make the following routes available only if connected 
+
+
+// Page to display the form to add a tshirt
+router.get('/add-tshirt', (req,res,next)=>{
+  res.render('add-tshirt')
+})
+
+
+// Page to handle the form submission and add a tshirt
+router.post('/add-tshirt', (req,res,next)=>{
+  Tshirt.create({
+    name: req.body.name,
+    pictureUrl: req.body.pictureUrl,
+    price: req.body.price,
+  })
+  .then(() => {
+    res.redirect('/tshirts')
+  })
+  .catch(next)
+})
+
+// Page to see the tshirts of the connected person
+router.get('/my-tshirts', (req,res,next)=>{
+  Tshirt.find() // TODO: change the filter to only show the right tshirts
+  .then(tshirts => {
+    res.render('tshirts', {tshirts})
+  })
+  .catch(next)
+})
+
+module.exports = router
